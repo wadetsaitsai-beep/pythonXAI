a6f67c1bd77d9ae1fbe757e02014a505249b734a
wadetsaitsai-beep <wadetsaitsai@gmail.com>
2026-07-13 16:52:59 +0800

    新增字串格式化範例，展示如何使用f-string顯示變數內容

--- pages/class1-1.py
+++ pages/class1-1.py
@@ -42,3 +42,9 @@ print(2 ** 3) #次方
 # 字串運算，+、*
 print("hello" + "world") #字串相加
 print("hello" * 3) #字串乘法
+
+# 字串格式化
+name = "apple"
+age = 18
+print(f"Hello, my name is {name}, I'm {age} years old.")  # f-string
+# 可以將變數或其他型態的資料放到f字串裡面的{}，這樣就可以在字串中顯示
