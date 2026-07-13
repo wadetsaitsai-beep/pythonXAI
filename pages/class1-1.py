207b4614ed4077dc05631cfd078d4b4c6105059b
wadetsaitsai-beep <wadetsaitsai@gmail.com>
2026-07-13 16:44:28 +0800

    新增 class1-1.py，包含基本的 Python 語法範例與註解

---
+++ pages/class1-1.py
@@ -0,0 +1,44 @@
+print("hello world!")
+print("蔡承佑")
+print("不要低頭\n雙下巴會出來")
+ 
+'''
+ 這是多行註解
+'''
+
+# 這是單行註解
+print("hello world!") #print是在終端機顯示的指令
+# ctrl + / 可以快速註解或取消註解
+
+# 基本型態
+print(1) # int這是整數, -1,0,1,2
+print(1.0) # float這是浮點數
+print(1.234) # float這是浮點數
+print("APPLE") # string這是字串 "sad123", '1'
+print(True) # bool這是布林值 True/False
+print(False) # bool這是布林值 True/False
+
+# 變數
+a = 10 #新增一個儲存空間並取名為a, "="的功能是將右邊的值10存入左邊的a
+print(a) # 在終端機顯示a所存的值
+a = "apple" #將a的值改為"apple"
+print(a) # 在終端機顯示a所存的值
+
+# 運算子
+print(1 + 1) #加法
+print(1 - 1) #減法
+print(1 * 1) #乘法
+print(1 / 1) #除法
+print(1 // 1) #整除
+print(1 % 1) #取餘數
+print(2 ** 3) #次方
+
+# 優先順序
+# 1. () 括號
+# 2. ** 次方
+# 3. * / // % 乘 除 取商 取餘數
+# 4. + - 加 減
+
+# 字串運算，+、*
+print("hello" + "world") #字串相加
+print("hello" * 3) #字串乘法
